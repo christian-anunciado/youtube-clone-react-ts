c16032e7b298afbea4288662f6e326df1b72fe79
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-02-27 01:29:10 +0800

    file cleanup

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,12 +1,12 @@
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import express, { Application, NextFunction, Request, Response } from 'express';
-import userRouter from './routes/usersRoute';
-import videosRouter from './routes/videosRoute';
-import commentsRouter from './routes/commentsRoute';
-import authController from './routes/authRoute';
-import { ErrorWithStatus } from './msc/types';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+import express, { Application, NextFunction, Request, Response } from "express";
+import userRouter from "./routes/usersRoute";
+import videosRouter from "./routes/videosRoute";
+import commentsRouter from "./routes/commentsRoute";
+import authController from "./routes/authRoute";
+import { ErrorWithStatus } from "./msc/types";
 
 dotenv.config();
 
@@ -16,37 +16,51 @@ const port = process.env.PORT;
 
 const mongoURL = process.env.MONGO_TEST as string;
 
-
-const connect = () => {
-    mongoose.connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true }, () => {
-        console.log('Connected to MongoDB!')
-    });
+const connect = async () => {
+  try {
+    await mongoose.connect(
+      mongoURL,
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      },
+      () => {
+        console.log("Connected to MongoDB!");
+      }
+    );
+  } catch (err) {
+    console.log(err);
+    process.exit(1);
+  }
 };
 
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(express.json());
-app.use("/api/users", userRouter)
-app.use("/api/videos", videosRouter)
-app.use("/api/comments", commentsRouter)
-app.use("/api/auth", authController)
+app.use("/api/users", userRouter);
+app.use("/api/videos", videosRouter);
+app.use("/api/comments", commentsRouter);
+app.use("/api/auth", authController);
 
-app.use((err: ErrorWithStatus, req: Request, res: Response, next: NextFunction): void => {
+app.use(
+  (
+    err: ErrorWithStatus,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ): void => {
     const status = err?.status || 500;
-    const message = err.message || 'Something went wrong';
+    const message = err.message || "Something went wrong";
     res.status(status).json({
-        message,
-        status,
-        success: false,
+      message,
+      status,
+      success: false,
     });
-
-});
+  }
+);
 
 app.listen(port, () => {
-    connect();
-    console.log(`Listening to port ${port}...`);
+  connect();
+  console.log(`Listening to port ${port}...`);
 });
-
-
-
-
-
