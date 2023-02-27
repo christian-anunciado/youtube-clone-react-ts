e37fc6b4b99f9df811742d7a8bcc2780cd8db971
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-02-28 00:08:28 +0800

    moved authentication methods to preHook schema

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -1,4 +1,7 @@
 import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+import { NextFunction } from "express";
+import { createError } from "../handlers/error";
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
@@ -38,6 +41,46 @@ const userSchema = new Schema(
     timestamps: true,
   }
 );
+
+userSchema.pre("save", async function (next: NextFunction) {
+  const user = this;
+  if (!user.isModified("password")) {
+    return next();
+  }
+
+  try {
+    const salt = bcrypt.genSaltSync(
+      parseInt(process.env.SALT_ROUNDS as string)
+    );
+
+    const hash = bcrypt.hashSync(user.password, salt);
+
+    user.password = hash;
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.statics.findByCredentials = async function (
+  name: string,
+  password: string,
+  next: NextFunction
+) {
+  // TODO: Find user by name
+  const user = await User.findOne({ name });
+
+  if (!user) {
+    next(createError(404, "User not found!"));
+  }
+
+  const isMatch = bcrypt.compareSync(password, user.password);
+
+  if (!isMatch) {
+    next(createError(401, "Wrong password!"));
+  }
+
+  return user;
+};
 
 const User = mongoose.model("User", userSchema);
 
--- server/src/msc/types.ts
+++ server/src/msc/types.ts
@@ -1,18 +1,23 @@
 import { Request } from "express";
-import User from '../models/User';
+import User from "../models/User";
 
 export interface ErrorWithStatus extends Error {
-    status?: number;
+  status?: number;
 }
 
 export interface RequestWithUser extends Request {
-    user?: SignedToken;
+  user?: SignedToken;
 }
 
 export interface Token {
-    id: string;
-    iat: number;
-    exp: number;
+  id: string;
+  iat: number;
+  exp: number;
 }
 
 export type SignedToken = Partial<Token>;
+
+export type LoginParams = {
+  name: string;
+  password: string;
+};
