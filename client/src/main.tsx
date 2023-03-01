f45a38efcf14fa843e8aaf89fad955890bedc7db
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-03-02 01:57:00 +0800

    added test toggle for darkMode using redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Provider } from "react-redux/es/exports";
+import { PersistGate } from "redux-persist/integration/react";
+import App from "./App";
+import "./index.css";
+import { persistor, store } from "./redux/store";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>
+);
