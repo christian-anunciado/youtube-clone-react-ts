a9c5cb170934520d03dca0574f7f02fdcdf591d2
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-03-03 01:58:41 +0800

    fixed some behaviors on menu

--- client/src/components/Menu/MenuItem.tsx
+++ client/src/components/Menu/MenuItem.tsx
@@ -10,7 +10,9 @@ type Props = {
 function MenuItem(props: Props) {
   return (
     <div
-      className={`flex min-w-full cursor-pointer items-center gap-5 rounded-lg py-1.5 px-3 text-sm hover:bg-soft dark:hover:bg-darkTextSoft dark:hover:text-darkText`}
+      className={`mr-3 flex min-w-0 cursor-pointer items-center gap-5 rounded-lg py-2 px-3 text-sm hover:bg-soft dark:hover:bg-darkSoft dark:hover:text-darkText ${
+        props.isActive && "bg-soft dark:bg-darkSoft"
+      }`}
       onClick={() => props.onClick?.()}
     >
       {props.isActive ? <>{props.iconActive}</> : <>{props.iconDefault}</>}
