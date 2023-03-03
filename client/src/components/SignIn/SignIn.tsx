44051204dd203db910dce082f09b5b0f6c99f362
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-03-03 23:40:32 +0800

    added changes to sign and navbar colors

--- client/src/components/SignIn/SignIn.tsx
+++ client/src/components/SignIn/SignIn.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 function SignIn({}: Props) {
   return (
-    <button className="flex h-fit w-max items-center justify-center gap-1 rounded-xl border border-blueBorder py-1 px-5 text-sm uppercase text-blueBorder hover:bg-blueSoft dark:hover:bg-darkBlueSoft">
+    <button className="flex h-fit w-max items-center justify-center gap-1 rounded-xl border border-blueBorder py-1 px-4 text-sm font-medium uppercase text-blueBorder hover:bg-blueSoft dark:hover:bg-darkBlueSoft">
       <IoPersonCircleOutline size={"1.3em"} />
       Sign In
     </button>
