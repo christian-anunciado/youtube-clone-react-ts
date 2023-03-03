3c47d813f8cb221587ec4df98cf3cd22bb6379e4
Christian Jay Anunciado <65912028+SkrowRepap@users.noreply.github.com>
2023-03-03 23:26:42 +0800

    added navigation for main content

--- client/src/components/Main/Main.tsx
+++ client/src/components/Main/Main.tsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Navbar from "../Navbar/Navbar";
 
 type Props = {};
 
 function Main({}: Props) {
-  return <div className="flex-3">Main</div>;
+  return (
+    <div className="flex-[3] text-text dark:text-darkText">
+      <Navbar />
+      Main
+    </div>
+  );
 }
 
 export default Main;
